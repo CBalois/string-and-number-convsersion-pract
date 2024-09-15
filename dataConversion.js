ebc49a3063f548ae2a1229e6b6d0641eb79381a9
Camelle <baloiscamelle@gmail.com>
2024-09-15 14:33:14 +0800

    Create dataConversion.js

---
+++ dataConversion.js
@@ -0,0 +1,20 @@
+let price = "200";
+
+let b = parseInt(price);
+console.log("Using parseInt():", b);
+let a = Number(price);
+console.log("Using Number():", a);
+let l  = +price;
+console.log("Using + operator:", l);
+
+
+let distance = 150;
+let o = distance.toString();
+console.log("Using toString():", o);
+let i = String(distance);
+console.log("Using String():", i);
+
+
+let weight = "72.5";
+let s = parseFloat(weight);
+console.log("Using parseFloat():", s);
